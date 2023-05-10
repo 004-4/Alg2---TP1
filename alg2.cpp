71cc8764a07efc89eacf9a42ea52dd60863c409d
004-4 <80779444+004-4@users.noreply.github.com>
2023-05-09 21:08:25 -0300

    Update alg2.cpp

--- alg2.cpp
+++ alg2.cpp
@@ -57,7 +57,7 @@ void descomprime (string e, string s){
         else break;
     }
     texto.push_back(texto[numero]);
-    for (int i = 1; i < texto.size(int); i++){
+    for (int i = 1; i < (int)texto.size(); i++){
         texto[0] += texto[i];
     }
     saida << texto[0];
